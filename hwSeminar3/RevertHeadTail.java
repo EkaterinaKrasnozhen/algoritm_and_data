69c894d9c16dfe37066c2afcc0f469108128478c
kate <01redcat@mail.ru>
2023-04-14 16:58:25 +0300

    node, del, find, revert, add

---
+++ hwSeminar3/RevertHeadTail.java
@@ -0,0 +1,30 @@
+package hwSeminar3;
+
+public class RevertHeadTail {
+    Node head;
+    Node tail;
+
+    public void revert(){
+        Node currNode = head;
+        while(currNode != null){
+            Node next = currNode.next;
+            Node previous = currNode.previous;
+            currNode.next = previous;
+            currNode.previous = next;
+            if (previous == null){
+                tail = currNode;
+            }
+            if (next == null){
+                head = currNode;
+            }
+            currNode = next;
+        }
+    }
+
+    public class Node {
+    
+        int value;
+        Node next;
+        Node previous;
+    }
+}
